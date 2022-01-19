3eeba1a8da70060a7ebb4ca2910b750fe31bca99
webdevprojcs <98015004+webdevprojcs@users.noreply.github.com>
2022-01-20 06:23:59 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ let navbar = $(".navbar");
 
 $(window).scroll(function () {
   // get the complete hight of window
-  let oTop = $(".section-2").offset().top - window.innerHeight;
+  let oTop = $(".section-3").offset().top - window.innerHeight;
   if ($(window).scrollTop() > oTop) {
     navbar.addClass("sticky");
   } else {
